ff3374a76286d5f836567f09ca5152a873414482
DT7Solutions <developer@dt7solutions.com>
2022-11-09 10:10:27 +0530

    fixed changes

--- Sumafilmyart/Sumafilmyart/settings.py
+++ Sumafilmyart/Sumafilmyart/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-dl)x$b60)uiuz3$t)uh-woh%8yiv0fjhy@d-gy$247%h^px)z&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,12 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [STATIC_DIR,]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Sumafilmyart/static/js/portfolio.js
+++ Sumafilmyart/static/js/portfolio.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".port_video_slider", {
-    slidesPerView:2,
+    slidesPerView:3,
         spaceBetween: 30,
         freeMode: true,
         // pagination: {
