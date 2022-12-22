5eb1e8a80c6828eb0400c7152de052c9bc79ddee
DT7Solutions <developer@dt7solutions.com>
2022-12-22 13:03:19 +0530

    banner changes

--- Sumafilmyart/static/js/init.js
+++ Sumafilmyart/static/js/init.js
@@ -89,18 +89,18 @@ function dizme_tm_trigger_menu(){
 	var mobileMenu		= jQuery('.dizme_tm_mobile_menu .dropdown');
 	var mobileMenuList	= jQuery('.dizme_tm_mobile_menu .dropdown .dropdown_inner ul li a');
 
-	// hamburger.on('click',function(){
-	// 	var element 	= jQuery(this);
+	hamburger.on('click',function(){
+		var element 	= jQuery(this);
 
-	// 	if(element.hasClass('is-active')){
-	// 		element.removeClass('is-active');
-	// 		mobileMenu.slideUp();
-	// 	}else{
-	// 		element.addClass('is-active');
-	// 		mobileMenu.slideDown();
-	// 	}
-	// 	return false;
-	// });
+		if(element.hasClass('is-active')){
+			element.removeClass('is-active');
+			mobileMenu.slideUp();
+		}else{
+			element.addClass('is-active');
+			mobileMenu.slideDown();
+		}
+		return false;
+	});
 	
 	mobileMenuList.on('click',function(){
 		jQuery('.trigger .hamburger').removeClass('is-active');
