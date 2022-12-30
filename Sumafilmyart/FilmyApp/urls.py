116b70c7d8c50235fef5d5dc36e2740d6497fbd9
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2022-12-30 02:01:15 -0800

    sumafilmyarts  changes

--- Sumafilmyart/FilmyApp/urls.py
+++ Sumafilmyart/FilmyApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from FilmyApp import views
 urlpatterns = [
     path('', views.home),
-    path('about/', views.about,name='about'),
+    path('aboutus/', views.about,name='aboutus'),
     path('portfolio/', views.portfolio,name='portfolio'),
     path('news_events/', views.news_events,name='news_events'),
     path('contact/', views.contact,name='contact'),
