4fe58ef5445629c636512e0a42fd2fa98b2012a7
DT7Solutions <developer@dt7solutions.com>
2023-01-02 12:36:36 +0530

    fixed form elements

---
+++ Sumafilmyart/static/collabrations/collabration.js
@@ -0,0 +1,43 @@
+
+
+function yesnoCheck(checkbox) {
+    if(checkbox.checked == true){
+        document.getElementById("ifYes").style.display = "block";
+        // document.getElementById("submit").removeAttribute("disabled");
+    }else{
+        // document.getElementById("submit").setAttribute("disabled", "disabled");
+        document.getElementById("ifYes").style.display = "none";
+   }
+}
+
+
+
+
+
+
+var AndraPradesh = ["TV Show ","Web Series ","Short Film","Film","Youtube Video"];
+var ArunachalPradesh = [""];
+
+
+$("#inputState").change(function(){
+  var StateSelected = $(this).val();
+  var optionsList;
+  var htmlString = "";
+
+  switch (StateSelected) {
+    case "Andra Pradesh":
+        optionsList = AndraPradesh;
+        break;
+    case "Arunachal Pradesh":
+        optionsList = ArunachalPradesh;
+        break;
+    
+}
+
+
+  for(var i = 0; i < optionsList.length; i++){
+    htmlString = htmlString+"<option value='"+ optionsList[i] +"'>"+ optionsList[i] +"</option>";
+  }
+  $("#inputDistrict").html(htmlString);
+
+});
