87828126649e95e99433006b88e60afabe0cd4c2
DT7Solutions <developer@dt7solutions.com>
2023-01-12 18:44:32 +0530

    fixed all issues and email

--- Sumafilmyart/FilmyApp/views.py
+++ Sumafilmyart/FilmyApp/views.py
@@ -4,7 +4,7 @@ from FilmyApp.models import ContactData, Application,Ideas,Sponsorship,Collabora
 from django.contrib import messages
 from django.core.mail import send_mail,EmailMessage
 from django.conf import settings
-import io
+import os
 # Create your views here.
 def home(request):
     return render (request,"uifiles/home.html",{'navbar':'home'})
@@ -51,6 +51,20 @@ def sponsorship(request):
         up_file = request.FILES['file']
         oApplication = Sponsorship(FirstName=firstname,LastName=lastname,Email=email,Phone=phone,Brand_Agency=brand_agency ,Industry=industry ,Kind_Sponcer=sponcer_kind,file=up_file ,Sponcer_Type=sponcer_type)
         oApplication.save()
+        
+        subject="sumfilmyart"
+        message =f'hi {firstname} Your Application has been submited Sucessfully'
+        
+        try:
+            # bio_file = io.BytesIO(up_file.read().encode('utf8'))
+            mail = EmailMessage(subject,message, settings.EMAIL_HOST_USER , [email,'manideep723@gmail.com'])
+            # mail.attach_file('Registrations_form.pdf')
+            mail.attach(up_file.name, up_file.read(), up_file.content_type)
+            mail.send()
+           
+        except:
+            messages.ERROR(request,'sending fail')
+
     return render (request,"uifiles/sponsorship.html",{'navbar':'sponsorship'})
  
 #enquiries
@@ -95,10 +109,7 @@ def show_page_1(request):
 def page_not_found_view(request, exception):
     return render(request, 'uifiles/404.html', status=404)
 
-#contact form submision  or your ideas form #
-
-# from django.views.decorators.csrf import csrf_exempt
-# @csrf_exempt
+#your ideas form #
 def contact(request):
     if request.method == "POST":
         name = request.POST.get('fname',"")
@@ -110,54 +121,54 @@ def contact(request):
         oContactinfo = Ideas(Name=name,Email=email,Phone=phone,Subject=subject,file = up_file)
         oContactinfo.save()
 
-        sucess =f'hi {name} Your message has been received, We will contact you soon'
-        
-        # message ='''
-        # Subject:{}
-        # Message:{}
-        # From:{}
-        # '''.format(subject,message,email)
-        # try:
-        #     send_mail(subject, message,'noreplaybadugudinesh94@gmail.com',recipient_list=['badugudinesh94@gmail.com']) 
-        #     messages.success(request,sucess)
-        # except:
-        #     messages.error(request,'Your message has been failed, Please Try Agian')
-        #return redirect("/")
+        subject="sumfilmyart"
+        message =f'hi {name} Your Application has been submited Sucessfully'
+
+        try:
+            # bio_file = io.BytesIO(up_file.read().encode('utf8'))
+            mail = EmailMessage(subject,message, settings.EMAIL_HOST_USER , [email,'manideep723@gmail.com'])
+            # mail.attach_file('Registrations_form.pdf')
+            mail.attach(up_file.name, up_file.read(), up_file.content_type)
+            mail.send()
+           
+        except:
+            messages.ERROR(request,'sending fail')
+     
     return render (request,"uifiles/contact.html",{'navbar':'contact'})
     
 
 
 
 
-def Apply_job(request):
-    if request.method == "POST":
-        name = request.POST.get('name',"")
-        email = request.POST.get('email',"")
-        phone = request.POST.get('phone',"")
-        address = request.POST.get('address',"")
-        experinces = request.POST.get('inpuexperices',"")
-        message = request.POST.get('message',"")
+# def Apply_job(request):
+#     if request.method == "POST":
+#         name = request.POST.get('name',"")
+#         email = request.POST.get('email',"")
+#         phone = request.POST.get('phone',"")
+#         address = request.POST.get('address',"")
+#         experinces = request.POST.get('inpuexperices',"")
+#         message = request.POST.get('message',"")
         # file = request.POST.get('message',"")
-        up_file = request.FILES['file']
-        tr_check = request.POST.get('check'," ")
-        agrement=''
-        if tr_check =="on":
-            agrement = 'True'
-        oApplication = Application(Name=name,Email=email,Phone=phone,Address=address,Experience =experinces ,Message=message, file=up_file ,Term_check=agrement)
-        oApplication.save()
-        subject="sumfilmyart"
-        sucess =f'hi {name} Your Application has been submited Sucessfully'
+        # up_file = request.FILES['file']
+        # tr_check = request.POST.get('check'," ")
+        # agrement=''
+        # if tr_check =="on":
+        #     agrement = 'True'
+        # oApplication = Application(Name=name,Email=email,Phone=phone,Address=address,Experience =experinces ,Message=message, file=up_file ,Term_check=agrement)
+        # oApplication.save()
+        # subject="sumfilmyart"
+        # sucess =f'hi {name} Your Application has been submited Sucessfully'
         
-        try:
+        # try:
             # bio_file = io.BytesIO(up_file.read().encode('utf8'))
-            mail = EmailMessage(subject,message, settings.EMAIL_HOST_USER , [email])
-            mail.attach(up_file.name, up_file.read(), up_file.content_type )
-            mail.send()
-            messages.success(request,sucess)
-        except:
-            messages.ERROR(request,'sending fail')
+    #         mail = EmailMessage(subject,message, settings.EMAIL_HOST_USER , [email])
+    #         mail.attach(up_file.name, up_file.read(), up_file.content_type )
+    #         mail.send()
+    #         messages.success(request,sucess)
+    #     except:
+    #         messages.ERROR(request,'sending fail')
                     
-    return render (request, "uifiles/carrerpages/applyform.html")
+    # return render (request, "uifiles/carrerpages/applyform.html")
 
 
 
