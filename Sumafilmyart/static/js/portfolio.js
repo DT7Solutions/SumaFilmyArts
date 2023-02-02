d467bfd0186a36ab987011f743dae9f4ce67c6cb
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-02-02 05:15:35 -0800

    final-changes

--- Sumafilmyart/static/js/portfolio.js
+++ Sumafilmyart/static/js/portfolio.js
@@ -25,7 +25,7 @@ var swiper = new Swiper(".port_video_slider1", {
           },
           // when window width is >= 640px
           640: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             
           }
         }
@@ -78,6 +78,7 @@ var swiper = new Swiper(".port_video_slider1", {
       }
   });
 
+// 
 
 
 
